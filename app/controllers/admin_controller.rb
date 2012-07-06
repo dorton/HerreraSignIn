6f61a6ca41ff3d8ed59e60229a62e2f532b35aa8
Brian Dorton <dorton@gmail.com>
2012-07-06 17:14:32 -0500

    renamed:    app/assets/stylesheets/scaffolds.css.scss -> app/assets/stylesheets/1scaffolds.css.scss 	modified:   app/assets/stylesheets/bootstrap_and_overrides.css.less 	modified:   app/controllers/admin_controller.rb 	modified:   app/views/admin/index.html.erb 	modified:   app/views/hisd_employees/new.html.erb 	modified:   app/views/home/index.html.erb 	modified:   app/views/layouts/application.html.erb 	deleted:    app/views/layouts/backup_header.txt 	modified:   app/views/student_check_outs/new.html.erb 	modified:   app/views/visitors/new.html.erb 	deleted:    public/index.html 	modified:   public/robots.txt

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,28 @@
 class AdminController < ApplicationController
-  def index
+ def index
+    @visitors = Visitor.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @visitors }
+    end
+  end
+
+  def indexa
+    @student_check_outs = StudentCheckOut.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @student_check_outs }
+    end
+  end
+
+  def indexb
+    @hisd_employees = HisdEmployee.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @hisd_employees }
+    end
   end
 end
