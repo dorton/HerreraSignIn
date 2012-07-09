231ceaac49cd5c4581797fd68c62acc9c444c797
Brian Dorton <dorton@gmail.com>
2012-07-08 22:40:42 -0500

    authenticate and test forms

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,32 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def new
+    @visitor = Visitor.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @visitor }
+    end
+  end
+
+  def new
+    @student_check_out = StudentCheckOut.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @student_check_out }
+    end
+  end
+
+  def new
+    @hisd_employee = HisdEmployee.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @hisd_employee }
+    end
+  end
+
 end
